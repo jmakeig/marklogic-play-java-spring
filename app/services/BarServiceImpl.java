676a800481e16e2eb7669fd3d5f00be4a7a0bd02
Justin Makeig <jmpublic@makeig.com>
2014-01-21 15:02:24 -0800

    Initial commit with resolved unused import warnings.

---
+++ app/services/BarServiceImpl.java
@@ -0,0 +1,34 @@
+package services;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaQuery;
+
+import models.Bar;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BarServiceImpl implements BarService {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Override
+    public void addBar(Bar bar) {
+        em.persist(bar);
+    }
+
+    @Override
+    public List<Bar> getAllBars() {
+        CriteriaQuery<Bar> c = em.getCriteriaBuilder().createQuery(Bar.class);
+        c.from(Bar.class);
+        return em.createQuery(c).getResultList();
+    }
+
+}
